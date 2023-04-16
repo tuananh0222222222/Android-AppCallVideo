908f79d11c4be4100b70ab221bf394885cb29c1a
khuattuananh <75654034+tuananh0222222222@users.noreply.github.com>
2023-04-16 14:21:12 +0700

    bai9 part1

--- app/src/main/java/com/demo/appvideov2/utillities/Constants.java
+++ app/src/main/java/com/demo/appvideov2/utillities/Constants.java
@@ -1,5 +1,6 @@
 package com.demo.appvideov2.utillities;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Constants {
@@ -12,6 +13,25 @@ public class Constants {
     public  static  final  String KEY_IS_SIGNED_IN = "isSignedIn";
     public  static  final  String KEY_USER_ID = "user_id";
     public  static  final  String KEY_FCM_TOKEN = "fcm_token";
+    public  static  final  String REMOTE_MSG_AUTHORIZATION = "authorization";
+    public  static  final  String REMOTE_MSG_CONTENT_TYPE = "Content-type";
+    public  static  final  String REMOTE_MSG_TYPE ="type";
+    public  static  final  String REMOTE_MSG_MEETING_TYPE ="meetingtype";
+    public  static  final  String REMOTE_MSG_INVITATION ="invitation";
+    public  static  final  String REMOTE_MSG_INVITER_TOKEN ="inviterToken";
+
+    public  static  final  String REMOTE_MSG_DATA ="data";
+    public  static  final  String REMOTE_MSG_REGISTRATION_IDS ="registration_ids";
+
+    public  static HashMap<String,String> getRemoteMessageHeader(){
+        HashMap<String,String> headers  = new HashMap<>();
+        headers.put(
+                Constants.REMOTE_MSG_AUTHORIZATION,
+                "key=BJS71Cn7znNw01rLPi-J5IWMNZkugRrkmkLWrnsLLPrXOMm1LgtEc3h4Wi1dAFStjHZODDBvkVQF3NSy5UiNyxU"
+        );
+        headers.put(Constants.REMOTE_MSG_CONTENT_TYPE,"application/json");
+        return  headers;
+    }
 
 
 
